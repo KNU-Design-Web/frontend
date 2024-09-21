7d3c9d4bee814082f52599b06e1c7a9a9d422b79
toothlessdev <toothless042@gmail.com>
2024-09-22 02:38:46 +0900

    feature: RootLayout 제거

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 
-import { RootLayout } from "@/layouts/RootLayout/RootLayout";
-
 import AboutPage from "@/pages/about/AboutPage";
 import ArchivePage from "@/pages/archive/ArchivePage";
 import GuestPage from "@/pages/guest/GuestPage";
@@ -9,17 +7,23 @@ import HomePage from "@/pages/home/HomePage";
 import ProfilePage from "@/pages/profile/ProfilePage";
 import ProjectPage from "@/pages/project/ProjectPage";
 
+import styled from "@emotion/styled";
+
+export const RouterWrapper = styled.div`
+    flex: 1;
+`;
+
 export const Router = () => {
     return (
-        <Routes>
-            <Route path="/" element={<RootLayout />}>
-                <Route index element={<HomePage />} />
-                <Route path="about" element={<AboutPage />} />
-                <Route path="project" element={<ProjectPage />} />
-                <Route path="profile" element={<ProfilePage />} />
-                <Route path="guest" element={<GuestPage />} />
-                <Route path="archive" element={<ArchivePage />} />
-            </Route>
-        </Routes>
+        <RouterWrapper>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/project" element={<ProjectPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/guest" element={<GuestPage />} />
+                <Route path="/archive" element={<ArchivePage />} />
+            </Routes>
+        </RouterWrapper>
     );
 };
--- src/layouts/RootLayout/RootLayout.style.ts
+++
@@ -1,5 +0,0 @@
-import styled from "@emotion/styled";
-
-export const RootLayoutWrapper = styled.main`
-    padding: 10px;
-`;
