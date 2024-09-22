cedea139007c1792ac78a3b82a4ff7c468083622
toothlessdev <toothless042@gmail.com>
2024-09-22 17:09:46 +0900

    todo: 애니메이션 최적화 필요 (레이아웃 변경)

--- src/layouts/Section/SectionRoute.style.ts
+++ src/layouts/Section/SectionRoute.style.ts
@@ -5,13 +5,32 @@ export const Wrapper = styled.div<{ active: boolean }>`
     width: 100%;
 
     max-width: ${(props) => (props.active ? "100%" : "0")};
+    will-change: flex-grow;
+    will-change: max-width;
+    transition: max-width 0.3s ease-in-out;
+
+    // TODO: 애니메이션 최적화 필요
+
+    /* transform: ${(props) => (props.active ? "scaleX(1)" : "scaleX(0)")};
+    transform-origin: left;
+    will-change: transform;
+    transition: transform 0.5s ease-in-out; */
+
+    /* max-width: ${(props) => (props.active ? "100%" : "0")};
+
+    transform: ${(props) => (props.active ? "scaleX(1)" : "scaleX(0)")};
+    transform-origin: left;
+
+    clip-path: ${(props) => (props.active ? "inset(0 0 0 0)" : "inset(0 100% 0 0)")};
+
+    will-change: clip-path;
+    transition:
+        transform 0.1s ease-in-out,
+        max-width 0.5s ease-in-out,
+        clip-path 0.3s ease-in-out; */
 
     overflow: hidden;
     white-space: nowrap;
-
-    will-change: flex-grow;
-    will-change: max-width;
-    transition: max-width 0.5s ease-in-out;
 `;
 
 export const Container = styled.div`
