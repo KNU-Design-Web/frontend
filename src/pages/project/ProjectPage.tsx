d63456ca626c0b6d6472000a1e6056820cb4cdd4
toothlessdev <toothless042@gmail.com>
2024-09-23 18:51:25 +0900

    feature: ProjectCard 컴폰넌트 추가

--- src/pages/project/ProjectPage.tsx
+++ src/pages/project/ProjectPage.tsx
@@ -1,9 +1,26 @@
+import { ProjectCard } from "@/components/project/Card/Card";
+import { ProjectCardContainer } from "@/components/project/CardContainer/CardContainer";
+
 import { Title } from "@/fonts/Title";
 
 export default function ProjectPage() {
     return (
         <>
             <Title>PROJECT</Title>
+
+            <ProjectCardContainer>
+                {Array.from({ length: 15 }).map((_, index) => {
+                    return (
+                        <ProjectCard
+                            key={index}
+                            id={index}
+                            imgSrc="https://placehold.co/320x320"
+                            title="프로젝트 제목"
+                            author="홍길동"
+                        />
+                    );
+                })}
+            </ProjectCardContainer>
         </>
     );
 }
