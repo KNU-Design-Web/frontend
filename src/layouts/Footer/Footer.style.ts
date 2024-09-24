476e073bc6591e2b68635ba114c3b1c941b4daca
toothlessdev <toothless042@gmail.com>
2024-09-24 09:26:43 +0900

    feature: Footer 컴포넌트 구현

--- src/layouts/Footer/Footer.style.ts
+++ src/layouts/Footer/Footer.style.ts
@@ -2,14 +2,47 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.footer`
     display: flex;
-    justify-content: center;
-    align-items: center;
+    flex-direction: column;
+    gap: 30px;
 
+    width: 100%;
     height: 360px;
-    margin-bottom: 50px;
 
+    margin-bottom: 50px;
     border-top: 2px solid #000;
     border-right: 1px solid #000;
+    padding: 50px 50px 80px 50px;
 
-    background-color: lightgray;
+    background-color: #fff;
+`;
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const Item = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+
+    span {
+        display: block;
+    }
+    &:first-child {
+        text-align: left;
+    }
+    &:last-child {
+        text-align: right;
+    }
+`;
+
+export const InfoContainer = styled.div``;
+
+export const InfoItem = styled.div`
+    display: flex;
+
+    span:first-child {
+        width: 60px;
+    }
 `;
