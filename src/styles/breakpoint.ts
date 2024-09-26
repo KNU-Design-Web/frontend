86630de3f50a11f2936f27dc908e7f2d7a00961a
toothlessdev <toothless042@gmail.com>
2024-09-26 15:52:34 +0900

    fix: 430px 보다 작은 디바이스에서도 모바일 뷰 동작하도록 수정

--- src/styles/breakpoint.ts
+++ src/styles/breakpoint.ts
@@ -1,4 +1,4 @@
 export const BREAKPOINT = {
     TABLET: `@media screen and (min-width: 768px) and (max-width: 1024px)`,
-    MOBILE: `@media screen and (min-width: 430px) and (max-width: 767px)`,
+    MOBILE: `@media screen and (max-width: 767px)`,
 };
