e5795bf5d3f127243386ddff6d1880497d40f150
toothlessdev <toothless042@gmail.com>
2024-10-09 04:07:31 +0900

    chore: mock data 추가

---
+++ src/services/data.ts
@@ -0,0 +1,21 @@
+export const data = [
+    {
+        id: 1,
+        author: {
+            name: {
+                ko: "",
+                en: "",
+            },
+            email: "",
+            instagram: "",
+            link: "",
+        },
+        project: {
+            thumbnail: "",
+            images: [""],
+            title: "",
+            hashtags: [""],
+            description: [""],
+        },
+    },
+] as const;
