949ba92d4eec8dbd91f3954a75f2c64ed1d14d09
toothlessdev <toothless042@gmail.com>
2024-10-09 04:07:08 +0900

    feature: ProfilePage 구현

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,9 +1,30 @@
-import { SamplePage } from "@/__mocks__/SamplePage";
+import { ProfileItem } from "@/components/profile/ProfileItem";
+import { ProfileList } from "@/components/profile/ProfileList";
+
+import { Title } from "@/fonts/Title";
 
 export default function ProfilePage() {
     return (
         <>
-            <SamplePage color={"deepskyblue"} text={"PROFILE"} />
+            <Title>PROFILE</Title>
+
+            <ProfileList>
+                {new Array(10).fill(undefined).map(() => {
+                    return (
+                        <ProfileItem
+                            koName="구지원"
+                            enName="JIWON KOO"
+                            email={""}
+                            instagram={""}
+                            link={""}
+                            profileImg={""}
+                            projectImg={""}
+                            projectId={""}
+                            projectTitle={""}
+                        />
+                    );
+                })}
+            </ProfileList>
         </>
     );
 }
