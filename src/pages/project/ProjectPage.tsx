30aeb3b9b234bb68c99ce16a55ff1e3ded5ae36f
toothlessdev <toothless042@gmail.com>
2024-10-11 00:11:01 +0900

    fix: ts double negation 오류 수정

--- src/pages/project/ProjectPage.tsx
+++ src/pages/project/ProjectPage.tsx
@@ -6,6 +6,6 @@ import ProjectListPage from "./ProjectListPage";
 export default function ProjectPage() {
     const [searchParams] = useSearchParams();
 
-    if (!!searchParams.get("id")) return <ProjectDetailPage />;
+    if (searchParams.get("id")) return <ProjectDetailPage />;
     else return <ProjectListPage />;
 }
