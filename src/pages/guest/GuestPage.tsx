e305a3ad7e659992c303ada41239391d7e46ff82
toothlessdev <toothless042@gmail.com>
2024-10-10 22:55:08 +0900

    feature: GuestPage 페이지 구현

--- src/pages/guest/GuestPage.tsx
+++ src/pages/guest/GuestPage.tsx
@@ -1,9 +1,31 @@
-import { SamplePage } from "@/__mocks__/SamplePage";
+import { GuestBookCard } from "@/components/guest/GuestBookCard/GuestBookCard";
+import { GuestBookCardContainer } from "@/components/guest/GuestBookCard/GuestBookCardContainer";
+import { GuestBookContainer } from "@/components/guest/GuestBookContainer/GuestBookContainer";
+import { GuestBookForm } from "@/components/guest/GuestBookForm/GuestBookForm";
+import { GuestBookInput } from "@/components/guest/GuestBookInput/GuestBookInput";
+import { GuestBookTextArea } from "@/components/guest/GuestBookTextArea/GuestBookTextArea";
 
 export default function GuestPage() {
     return (
         <>
-            <SamplePage color={"aliceblue"} text={"GUEST PAGE"} />
+            <GuestBookContainer>
+                <GuestBookForm>
+                    <GuestBookInput name="receiver" label="TO." width="300px" placeholder="받는사람" />
+                    <GuestBookTextArea
+                        placeholder="경북대학교 디자인학과 학생들에게&#10;따뜻한 응원의 한마디를 남겨주세요:)"
+                    />
+                    <GuestBookInput name="receiver" label="FROM." width="300px" placeholder="보내는사람" />
+                </GuestBookForm>
+
+                <GuestBookCardContainer>
+                    <GuestBookCard></GuestBookCard>
+                    <GuestBookCard></GuestBookCard>
+                    <GuestBookCard></GuestBookCard>
+                    <GuestBookCard></GuestBookCard>
+                    <GuestBookCard></GuestBookCard>
+                    <GuestBookCard></GuestBookCard>
+                </GuestBookCardContainer>
+            </GuestBookContainer>
         </>
     );
 }
