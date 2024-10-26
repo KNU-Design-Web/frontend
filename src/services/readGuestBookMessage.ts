b6aa83354ef79039b68e65cd1f282fab63e3f360
toothlessdev <toothless042@gmail.com>
2024-10-27 05:28:54 +0900

    fix: queryParams 를 axios 에서 처리

--- src/services/readGuestBookMessage.ts
+++ src/services/readGuestBookMessage.ts
@@ -1,8 +1,11 @@
 import { api } from "@/apps/config/axios";
 
 export const readGuestBookMessage = async (lastSortKey: string | null, take: number) => {
-    const response = await api.get<GuestBookResponse>(
-        `/guestbook?lastEvaluatedSk=${lastSortKey as string}&take=${take}`,
-    );
+    const response = await api.get<GuestBookResponse>("/guestbook", {
+        params: {
+            lastEvaluatedSK: lastSortKey,
+            take,
+        },
+    });
     return response;
 };
