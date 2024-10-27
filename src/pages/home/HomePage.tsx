7d963d552ab8f8f191bceede1c14fa3f9b34687f
toothlessdev <toothless042@gmail.com>
2024-10-27 23:26:24 +0900

    fix: 모바일 동영상 플레이어에서 재생되는 현상 해결 (playsInline 속성)

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,7 +3,13 @@ import * as HomePageStyles from "./HomePage.style";
 export default function HomePage() {
     return (
         <HomePageStyles.VideoContainer>
-            <HomePageStyles.Video src="https://images.knud2024.com/main/main-origin.webm" autoPlay loop muted />
+            <HomePageStyles.Video
+                src="https://images.knud2024.com/main/main-origin.webm"
+                autoPlay
+                loop
+                muted
+                playsInline
+            />
         </HomePageStyles.VideoContainer>
     );
 }
