ddb3b808ffa75f5d44a90a520b4de4b8062a8039
Dongpil Jo <ehdvlf001@knu.ac.kr>
2024-10-28 06:26:22 +0900

    fix: footer 반응형 수정

--- src/common/components/Footer/Footer.tsx
+++ src/common/components/Footer/Footer.tsx
@@ -1,62 +1,75 @@
 import * as FooterStyles from "./Footer.style";
-import { Text } from "@/common/components/Text/Text";
+import { Text as _Text } from "@/common/components/Text/Text";
+import styled from "@emotion/styled";
 
 export const Footer = () => {
     return (
         <FooterStyles.Wrapper>
             <FooterStyles.Container>
                 <FooterStyles.Item>
-                    <Text size="m" weight={700}>
+                    <Ltext size="m" weight={700}>
                         40th Graduation Exhibition
-                    </Text>
-                    <Text size="m" weight={700}>
+                    </Ltext>
+                    <Ltext size="m" weight={700}>
                         2024 Kyungpook National University
-                    </Text>
-                    <Text size="m" weight={700}>
+                    </Ltext>
+                    <Ltext size="m" weight={700}>
                         합니다. 디자인
-                    </Text>
+                    </Ltext>
                 </FooterStyles.Item>
 
                 <FooterStyles.Item>
-                    <Text size="s" weight={500} color="#909090">
+                    <Stext size="s" weight={500} color="#909090">
                         Instagram @knu_design_
-                    </Text>
-                    <Text size="s" weight={500} color="#909090">
+                    </Stext>
+                    <Stext size="s" weight={500} color="#909090">
                         T. 053-950-5694
-                    </Text>
-                    <Text size="s" weight={500} color="#909090">
+                    </Stext>
+                    <Stext size="s" weight={500} color="#909090">
                         https://vcd.knu.ac.kr
-                    </Text>
+                    </Stext>
                 </FooterStyles.Item>
             </FooterStyles.Container>
 
             <FooterStyles.InfoContainer>
                 <FooterStyles.InfoItem>
-                    <Text size="s" weight={700}>
+                    <Stext size="s" weight={700}>
                         장소
-                    </Text>
-                    <Text size="s" weight={500}>
+                    </Stext>
+                    <Stext size="s" weight={500}>
                         <span>북구 대학로 80 경북대학교</span> <span>대구캠퍼스 스페이스 나인</span>
-                    </Text>
+                    </Stext>
                 </FooterStyles.InfoItem>
                 <FooterStyles.InfoItem>
-                    <Text size="s" weight={700}>
+                    <Stext size="s" weight={700}>
                         일시
-                    </Text>
-                    <Text size="s" weight={500}>
+                    </Stext>
+                    <Stext size="s" weight={500}>
                         <span>2024. 10. 28 MON - </span>
                         <span>2024.11.09 SUN</span>
-                    </Text>
+                    </Stext>
                 </FooterStyles.InfoItem>
                 <FooterStyles.InfoItem>
-                    <Text size="s" weight={700}>
+                    <Stext size="s" weight={700}>
                         오프닝
-                    </Text>
-                    <Text size="s" weight={500}>
+                    </Stext>
+                    <Stext size="s" weight={500}>
                         2024. 10. 31 THU
-                    </Text>
+                    </Stext>
                 </FooterStyles.InfoItem>
             </FooterStyles.InfoContainer>
         </FooterStyles.Wrapper>
     );
 };
+
+const Ltext = styled(_Text)`
+    @media (max-width: 430px) {
+        font-size: 20px;
+    }
+`;
+
+const Stext = styled(_Text)`
+    @media (max-width: 430px) {
+        font-size: 16px;
+    }
+`;
