b08ebd05a7c1ae34a55758f7cb94fa16af09c281
toothlessdev <toothless042@gmail.com>
2024-10-28 03:33:44 +0900

    chore: 사용하지 않는 imports 제거

--- src/apps/Router.tsx
+++ src/apps/Router.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useLayoutEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { TransitionGroup } from "react-transition-group";
 
