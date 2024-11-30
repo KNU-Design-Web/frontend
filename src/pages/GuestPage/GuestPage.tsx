49e921ba045e5871404af2ff46d64da169299aa0
Dongpil Jo <ehdvlf001@knu.ac.kr>
2024-11-30 22:34:32 +0900

    remove: 불필요 코드 삭제

--- src/pages/GuestPage/GuestPage.tsx
+++ src/pages/GuestPage/GuestPage.tsx
@@ -1,55 +1,17 @@
 import { GuestBookCard } from "@/components/GuestBookPage/GuestBookCard/GuestBookCard";
 import { GuestBookCardContainer } from "@/components/GuestBookPage/GuestBookCard/GuestBookCardContainer";
 import { GuestBookContainer } from "@/components/GuestBookPage/GuestBookContainer/GuestBookContainer";
-import { GuestBookForm } from "@/components/GuestBookPage/GuestBookForm/GuestBookForm";
-import { GuestBookInput } from "@/components/GuestBookPage/GuestBookInput/GuestBookInput";
-import { GuestBookTextArea } from "@/components/GuestBookPage/GuestBookTextArea/GuestBookTextArea";
 
 import { useGuestBook } from "@/hooks/GuestBookPage/useGuestBook";
 
 import styled from "@emotion/styled";
 
 export default function GuestPage() {
-    const {
-        isLastPage,
-        guestBookData,
-        targetRef,
-        from,
-        to,
-        content,
-        handleFromChange,
-        handleToChange,
-        handleContentChange,
-        handleSubmit,
-    } = useGuestBook();
+    const { isLastPage, guestBookData, targetRef } = useGuestBook();
 
     return (
         <>
             <GuestBookContainer>
-                {/* <GuestBookForm>
-                    <GuestBookInput
-                        name="receiver"
-                        label="TO."
-                        width="100px"
-                        placeholder="받는사람"
-                        value={to}
-                        onChange={handleToChange}
-                    />
-                    <GuestBookTextArea
-                        placeholder="경북대학교 디자인학과 학생들에게 따뜻한 응원의 한마디를 남겨주세요:)"
-                        value={content}
-                        onChange={handleContentChange}
-                    />
-                    <GuestBookInput
-                        name="sender"
-                        label="FROM."
-                        width="105px"
-                        placeholder="보내는사람"
-                        value={from}
-                        onChange={handleFromChange}
-                    />
-                </GuestBookForm>
-                <AddButton onClick={handleSubmit}>메시지 전송</AddButton> */}
                 <GuestBookCardContainer>
                     {guestBookData.map((guestBookMessage) => {
                         return (
@@ -67,15 +29,6 @@ export default function GuestPage() {
         </>
     );
 }
-
-const AddButton = styled.button`
-    width: 100%;
-    height: 72px;
-    background-color: #9085ff;
-    font-size: 24px;
-    color: white;
-    font-weight: bold;
-`;
 
 const LoadingMessage = styled.div`
     text-align: center;
