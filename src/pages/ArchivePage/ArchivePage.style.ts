4f3b10faa771e737843a99c65abe0535cbfe43a1
toothlessdev <toothless042@gmail.com>
2025-01-03 04:54:53 +0900

    feature: ArchivePage 구현

--- src/pages/ArchivePage/ArchivePage.style.ts
+++ src/pages/ArchivePage/ArchivePage.style.ts
@@ -3,6 +3,10 @@ import styled from "@emotion/styled";
 
 export const Layout = styled.div`
     display: flex;
+    justify-content: center;
+    overflow-y: hidden;
+
+    height: 2000px;
 
     ${BREAKPOINT.MOBILE} {
         flex-direction: column;
--- src/pages/ArchivePage/ArchivePage.tsx
+++ src/pages/ArchivePage/ArchivePage.tsx
@@ -3,18 +3,20 @@ import { PhotoGrid } from "@/components/ArchivePage/PhotoGrid/PhotoGrid";
 
 import * as ArchivePageStyles from "./ArchivePage.style";
 import { archiveImagesBefore, archiveImagesAfter } from "@/apps/data";
+import { Title } from "@/common/components/Title/Title";
 
 export default function ArchivePage() {
     return (
         <>
+            <Title>ARCHIVE</Title>
             <ArchivePageStyles.Layout>
-                <PhotoGrid direction="column">
+                <PhotoGrid direction="up" speed={5}>
                     {archiveImagesBefore.map((imageUrl, index) => {
                         return <PhotoCard key={index} src={imageUrl} />;
                     })}
                 </PhotoGrid>
 
-                <PhotoGrid direction="column">
+                <PhotoGrid direction="down" speed={5}>
                     {archiveImagesAfter.map((imageUrl, index) => {
                         return <PhotoCard key={index} src={imageUrl} />;
                     })}
