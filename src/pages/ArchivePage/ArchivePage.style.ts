bf961480e30cb7a5663f97403b7517517225664c
toothlessdev <toothless042@gmail.com>
2025-01-04 01:07:48 +0900

    style: GridLayout 에 align-items center 적용

--- src/pages/ArchivePage/ArchivePage.style.ts
+++ src/pages/ArchivePage/ArchivePage.style.ts
@@ -4,6 +4,7 @@ import styled from "@emotion/styled";
 export const Layout = styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
     overflow-y: hidden;
 
     height: 2000px;
