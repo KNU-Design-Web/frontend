521689eeef1579e08c0954aaaba72d440a711c56
toothlessdev <toothless042@gmail.com>
2025-01-04 01:07:29 +0900

    fix: PhotoCardWrapper 를 사용해 LayoutShift 최소화

--- src/components/ArchivePage/PhotoCard/PhotoCard.style.ts
+++ src/components/ArchivePage/PhotoCard/PhotoCard.style.ts
@@ -1,12 +1,18 @@
 import { BREAKPOINT } from "@/apps/styles/breakpoint";
 import styled from "@emotion/styled";
 
+export const ImageWrapper = styled.div<{ width: string; height: string }>`
+    width: ${({ width }) => width};
+    height: ${({ height }) => height};
+
+    overflow: hidden;
+`;
+
 export const Image = styled.img`
     display: block;
     object-fit: cover;
 
     width: 100%;
-    max-width: 620px;
 
     ${BREAKPOINT.MOBILE} {
         height: 620px;
