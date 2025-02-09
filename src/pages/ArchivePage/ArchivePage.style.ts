b810bb867a8460e346e397dea71c92daf330a8e3
toothlessdev <toothless042@gmail.com>
2025-02-09 20:31:09 +0900

    feature: Archive 페이지에 그리드 추가

--- src/pages/ArchivePage/ArchivePage.style.ts
+++ src/pages/ArchivePage/ArchivePage.style.ts
@@ -2,12 +2,17 @@ import { BREAKPOINT } from "@/apps/styles/breakpoint";
 import styled from "@emotion/styled";
 
 export const Layout = styled.div`
+    position: relative;
+
     display: flex;
     justify-content: center;
     align-items: center;
     overflow-y: hidden;
 
+    max-width: 970px;
     height: 2000px;
+
+    margin: 0px auto;
 
     ${BREAKPOINT.MOBILE} {
         flex-direction: column;
