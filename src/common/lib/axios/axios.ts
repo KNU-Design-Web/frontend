ca9b47c02539ad448b5acaec138d2ac04cdce37c
toothlessdev <toothless042@gmail.com>
2025-06-04 10:43:22 +0900

    feature: axios retry 횟수 및 retry delay 설정

--- src/common/lib/axios/axios.ts
+++ src/common/lib/axios/axios.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
+import retry, { exponentialDelay, isNetworkOrIdempotentRequestError } from "axios-retry";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL as string,
+});
+
+retry(api, {
+    retries: 3,
+    retryDelay: exponentialDelay,
+    retryCondition: (error) => isNetworkOrIdempotentRequestError(error),
 });
